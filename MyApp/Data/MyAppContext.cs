a99177393eaaaf08d92e3b738625037d38308de9
KodcuAhmet <ahmet.cigil@tuta.com>
2025-03-07 20:44:59 +0100

    Project finished

--- MyApp/Data/MyAppContext.cs
+++ MyApp/Data/MyAppContext.cs
@@ -5,9 +5,41 @@ namespace MyApp.Data
 {
     public class MyAppContext : DbContext
     {
-        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options)
+        public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Item>().HasData(
+                new Item { Id = 4, Name = "microphone", Price = 40, SerialNumberId = 10 }
+                );
+            modelBuilder.Entity<SerialNumber>().HasData(
+                new SerialNumber { Id = 10, Name = "MIC10", ItemId = 4 }
+                );
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" },
+                new Category { Id = 2, Name = "Books" }
+                );
+            modelBuilder.Entity<ItemClient>().HasKey(
+                ic => new
+                {
+                    ic.ItemId,
+                    ic.ClientId
+                });
+            modelBuilder.Entity<ItemClient>()
+                .HasOne(i => i.Item)
+                .WithMany(ic => ic.ItemClients)
+                .HasForeignKey(i => i.ItemId);
+            modelBuilder.Entity<ItemClient>()
+                .HasOne(c => c.Client)
+                .WithMany(ic => ic.ItemClients)
+                .HasForeignKey(c => c.ClientId);
+
+            base.OnModelCreating(modelBuilder);
         }
+
         public DbSet<Item> Items { get; set; }
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ItemClient> ItemClients { get; set; }
     }
 }
